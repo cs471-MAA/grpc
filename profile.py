d96899b81a33fdaa3715b79d26100f18d1befa7a
Saheru <adriansagerlaganga@gmail.com>
2021-12-19 19:04:33 +0100

    created profile.py

---
+++ profile.py
@@ -0,0 +1,40 @@
+"""Cloudlab script to create experiment profile (cluster topology). It will only be used by Cloudlab to setup the cluster nodes."""
+
+# Import the Portal object.
+import geni.portal as portal
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+def create_node(ports, entrypoint):
+    # Add a Docker container to the request.
+    node = request.DockerContainer("node")
+
+    # Indicate the image is external
+    # node.docker_extimage = "saheru/grpc-benchmark:latest"
+
+    # Indicate the dockerfile for the benchmark
+    node.docker_dockerfile = "/local/repository/benchmark.dockerfile"
+
+    # entrypoint
+    node.docker_entrypoint = entrypoint
+
+    # can specify cores, ram and disk space
+    # node.cores =
+    # node.ram =
+    # node.disk =
+
+    # TODO: somehow we must be able to specify the ports!
+
+    return node
+
+create_node(ports='20001:10001', entrypoint="/app/cmake/build/mockDatabaseAsync 1 32 100000")
+create_node(ports='20003:10003', entrypoint="/app/cmake/build/sanitizationServiceAsync 1 32")
+create_node(ports='20002:10002', entrypoint="/app/cmake/build/messageServiceAsync 1 32")
+create_node(ports='', entrypoint="/app/cmake/build/clientAsync 100 TestUser 'Test Message' 100000 0.995")
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
