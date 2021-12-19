f114a862238bb0712fee8aa3dde001d11bf8c211
Adrien Remillieux <adrien.remillieux@epfl.ch>
2021-12-19 22:42:41 +0100

    sanitizationService without delay

---
+++ sanitizationService/sync/sanitizationService.h
@@ -0,0 +1,13 @@
+
+#include "mock_message_board.grpc.pb.h"
+
+
+// Logic and data behind the server's behavior.
+class sanitizationServiceImpl final : public mmb::sanitizationService::Service {
+public:
+    sanitizationServiceImpl();
+    ::grpc::Status sanitize_message(::grpc::ServerContext* context, const ::mmb::saveMessageRequest* request, ::mmb::saveMessageReply* response) override;
+
+private:
+    std::unique_ptr<mmb::mockDatabase::Stub> mockDatabaseStub_;
+};
